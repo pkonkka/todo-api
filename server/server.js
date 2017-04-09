49e0452c137b9300eb584dc46f84b2f86d2a724d
Petri Konkka <petrikonkka@gmail.com>
2017-04-09 16:05:33 +0300

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -31,11 +31,6 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
-});
-
-// GET /todos/12345
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -43,17 +38,34 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id)
-    .then((todo) => {
-      if (!todo) {
-        return res.status(404).send();
-      }
-      res.send({todo});
-    })
-    .catch(() => res.status(400).send());
-  
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  })
+  .catch(() => res.status(400).send());  
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => res.status(400).send());
+
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
 
 module.exports = {app};
 
