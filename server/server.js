ba71e4cc15a759cb7c87e0c164ff190768cb603e
Petri Konkka <petrikonkka@gmail.com>
2017-04-12 18:22:42 +0300

    Added header Access-Control-Allow-Origin

--- server/server.js
+++ server/server.js
@@ -15,6 +15,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // --- POST -----------------------------------------------------
 app.post('/todos', (req, res) => {
