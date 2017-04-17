d13bbfde77dafa816340681b9c42fabce2a59f2e
Petri Konkka <petrikonkka@gmail.com>
2017-04-17 13:38:32 +0300

    Access-Control-Allow headers commented out

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 // --- DELETE TODO ---------------------------------------------------
